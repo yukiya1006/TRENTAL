595b9c3852766b9959501d26541030e9e0da65d7
yukiya1006 <ymkyky1006@outlook.jp>
2022-04-07 12:49:37 +0000

    [fix]

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,51 +1,51 @@
-# class RoomsController < ApplicationController
-#   def show
-#   @room = Room.find(params[:id]) #ルーム情報の取得
-#     @message = Message.new #新規メッセージ投稿
-#     @messages = @room.messages #このルームのメッセージを全て取得
-#     if user_signed_in?
-#       if @room.user.id == current_user.id
-#         @trainer = @room.trainer
-#       else
-#         redirect_to "/"
-#       end
-#     elsif trainer_signed_in?
-#       if @room.trainer.id == current_trainer.id
-#         @user = @room.user
-#       else
-#         redirect_to "/"
-#       end
+class RoomsController < ApplicationController
+  def show
+  @room = Room.find(params[:id]) #ルーム情報の取得
+    @message = Message.new #新規メッセージ投稿
+    @messages = @room.messages #このルームのメッセージを全て取得
+    if user_signed_in?
+      if @room.user.id == current_user.id
+        @trainer = @room.trainer
+      else
+        redirect_to "/"
+      end
+    elsif trainer_signed_in?
+      if @room.trainer.id == current_trainer.id
+        @user = @room.user
+      else
+        redirect_to "/"
+      end
 
-#     else
-#       redirect_to "/"
-#     end
-#   end
+    else
+      redirect_to "/"
+    end
+  end
 
-#   def create
-#     if user_signed_in?
-#       #userがログインしてたらuser_idを, shopがログインしてたらshop_idを@roomにいれる
-#       @room = Room.new(room_shop_params)
-#       @room.user_id = current_user.id
-#     elsif trainer_signed_in?
-#       @room = Room.new(room_user_params)
-#       @room.trainer_id = current_trainer.id
-#     else
-#       redirect_to "/"
-#     end
+  def create
+    if user_signed_in?
+      #userがログインしてたらuser_idを, shopがログインしてたらshop_idを@roomにいれる
+      @room = Room.new(room_shop_params)
+      @room.user_id = current_user.id
+    elsif trainer_signed_in?
+      @room = Room.new(room_user_params)
+      @room.trainer_id = current_trainer.id
+    else
+      redirect_to "/"
+    end
 
-#     if @room.save
-#       redirect_to :action => "show", :id => @room.id
-#     else
-#       redirect_to "/"
-#     end
-#   end
+    if @room.save
+      redirect_to :action => "show", :id => @room.id
+    else
+      redirect_to "/"
+    end
+  end
 
-#   private
-#   def room_trainer_params
-#     params.require(:room).permit(:trainer_id)
-#   end
+  private
+  def room_trainer_params
+    params.require(:room).permit(:trainer_id)
+  end
 
-#   def room_user_params
-#     params.require(:room).permit(:user_id)
-#   end
-# end
+  def room_user_params
+    params.require(:room).permit(:user_id)
+  end
+end
