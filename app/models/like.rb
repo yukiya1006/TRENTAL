2e4eb1119ac003c58ab47c3a1de476ee4f9c30b2
yukiya1006 <ymkyky1006@outlook.jp>
2022-04-09 07:27:20 +0000

    [comment]

--- app/models/like.rb
+++ app/models/like.rb
@@ -4,7 +4,7 @@ class Like < ApplicationRecord
   
   belongs_to :user
   belongs_to :trainer
-  belongs_to :evaluate_user, class_name: User, foreign_key: :evaluate_user_id
+  belongs_to :evaluate_user, class_name: "User", foreign_key: :evaluate_user_id
   
   validates :evaluate_user_id, uniqueness: true
 end
