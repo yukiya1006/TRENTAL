776d3a74f79c91c0dbb5518bc23ddc1a262bdaf5
yukiya1006 <ymkyky1006@outlook.jp>
2022-04-17 17:20:21 +0000

    [comment]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
 
 protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image, :age, :gender, :activity_area, :training_history, :teaching_history, :qualification, :session_fee, :biography])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :name, :image, :age, :gender, :activity_area, :training_history, :teaching_history, :qualification, :session_fee, :biography])
   end
 end
